260a8e852013c67612db228147a6bfe07232dc19
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-02-25 23:11:17 +0100

    Read data from all connected DS18B2 sensors
    Read data from all sensors and send the information as a custom json document to IotHub

--- src/heating_tempflow_iotedge/modules/DS18B2TemperatureReader/Program.cs
+++ src/heating_tempflow_iotedge/modules/DS18B2TemperatureReader/Program.cs
@@ -1,6 +1,8 @@
 namespace DS18B2TemperatureReader
 {
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Runtime.InteropServices;
     using System.Runtime.Loader;
     using System.Security.Cryptography.X509Certificates;
@@ -10,47 +12,90 @@ namespace DS18B2TemperatureReader
     using System;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
     using Microsoft.Azure.Devices.Client;
+    using Newtonsoft.Json;
 
     class Program
     {
         static int counter;
 
-        static async Task Main (string[] args)
+        static async Task Main(string[] args)
         {
-            await Init ();
+          //  await Init();
 
             // Wait until the app unloads or is cancelled
-            var cts = new CancellationTokenSource ();
+            var cts = new CancellationTokenSource();
 
-            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel ();
-            Console.CancelKeyPress += (sender, cpe) => cts.Cancel ();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
 
-            MqttTransportSettings mqttSetting = new MqttTransportSettings (TransportType.Mqtt_Tcp_Only);
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
             ITransportSettings[] settings = { mqttSetting };
 
             // Open a connection to the Edge runtime
-            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync (settings);
-            await ioTHubModuleClient.OpenAsync ();
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await ioTHubModuleClient.OpenAsync();
 
             while (!cts.IsCancellationRequested)
             {
-                var content = File.ReadAllText ("/w1devices/28-0316856daaff/w1_slave");
-                var msg = new Message (System.Text.Encoding.UTF8.GetBytes (content));
+                var allSensors = Directory.GetDirectories("/w1devices", "28*");
 
-                await ioTHubModuleClient.SendEventAsync (msg);
-                await Task.Delay (30000);
+                var sensorReadTasks = new List<Task<SensorData>>();
+
+                foreach (var sensor in allSensors)
+                {
+                    sensorReadTasks.Add(ReadSensorDataAsync($"{sensor}/w1_slave", cts.Token));
+                }
+
+                await Task.WhenAll(sensorReadTasks);
+
+                await SendDataToIotHubAsync(sensorReadTasks.Select(t => t.Result), ioTHubModuleClient);
+
+                await Task.Delay(30000);
             }
 
-            await WhenCancelled (cts.Token);
+            await ioTHubModuleClient.CloseAsync();
+            await WhenCancelled(cts.Token);
+        }
+
+        private static async Task<SensorData> ReadSensorDataAsync(string sensor, CancellationToken cts)
+        {
+            var content = await File.ReadAllTextAsync(sensor, cts);
+
+            var temperature = GetTemperatureFromContent(content);
+
+            return new SensorData
+            {
+                SensorId = sensor,
+                    MeasurementDateTime = DateTime.Now,
+                    Temperature = temperature
+            };
+
+            double GetTemperatureFromContent(string sensorData)
+            {
+                int index = content.LastIndexOf(" t=");
+                return Convert.ToDouble(content.Substring(index + 3));
+            }
+        }
+
+        private static async Task SendDataToIotHubAsync(IEnumerable<SensorData> sensorData, ModuleClient moduleClient)
+        {
+            List<Message> messages = new List<Message>();
+
+            foreach (var measurement in sensorData)
+            {
+                messages.Add(new Message(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(measurement))));
+            }
+
+            await moduleClient.SendEventBatchAsync(messages);
         }
 
         /// <summary>
         /// Handles cleanup operations when app is cancelled or unloads
         /// </summary>
-        public static Task WhenCancelled (CancellationToken cancellationToken)
+        public static Task WhenCancelled(CancellationToken cancellationToken)
         {
-            var tcs = new TaskCompletionSource<bool> ();
-            cancellationToken.Register (s => ((TaskCompletionSource<bool>) s).SetResult (true), tcs);
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>) s).SetResult(true), tcs);
             return tcs.Task;
         }
 
@@ -58,18 +103,18 @@ namespace DS18B2TemperatureReader
         /// Initializes the ModuleClient and sets up the callback to receive
         /// messages containing temperature information
         /// </summary>
-        static async Task Init ()
+        static async Task Init()
         {
-            MqttTransportSettings mqttSetting = new MqttTransportSettings (TransportType.Mqtt_Tcp_Only);
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
             ITransportSettings[] settings = { mqttSetting };
 
             // Open a connection to the Edge runtime
-            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync (settings);
-            await ioTHubModuleClient.OpenAsync ();
-            Console.WriteLine ("IoT Hub module client initialized.");
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
 
             // Register callback to be called when a message is received by the module
-            await ioTHubModuleClient.SetInputMessageHandlerAsync ("input1", PipeMessage, ioTHubModuleClient);
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
         }
 
         /// <summary>
@@ -77,29 +122,29 @@ namespace DS18B2TemperatureReader
         /// It just pipe the messages without any change.
         /// It prints all the incoming messages.
         /// </summary>
-        static async Task<MessageResponse> PipeMessage (Message message, object userContext)
+        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
         {
-            int counterValue = Interlocked.Increment (ref counter);
+            int counterValue = Interlocked.Increment(ref counter);
 
             var moduleClient = userContext as ModuleClient;
             if (moduleClient == null)
             {
-                throw new InvalidOperationException ("UserContext doesn't contain " + "expected values");
+                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
             }
 
-            byte[] messageBytes = message.GetBytes ();
-            string messageString = Encoding.UTF8.GetString (messageBytes);
-            Console.WriteLine ($"Received message: {counterValue}, Body: [{messageString}]");
+            byte[] messageBytes = message.GetBytes();
+            string messageString = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
 
-            if (!string.IsNullOrEmpty (messageString))
+            if (!string.IsNullOrEmpty(messageString))
             {
-                var pipeMessage = new Message (messageBytes);
+                var pipeMessage = new Message(messageBytes);
                 foreach (var prop in message.Properties)
                 {
-                    pipeMessage.Properties.Add (prop.Key, prop.Value);
+                    pipeMessage.Properties.Add(prop.Key, prop.Value);
                 }
-                await moduleClient.SendEventAsync ("output1", pipeMessage);
-                Console.WriteLine ("Received message sent");
+                await moduleClient.SendEventAsync("output1", pipeMessage);
+                Console.WriteLine("Received message sent");
             }
             return MessageResponse.Completed;
         }
---
+++ src/heating_tempflow_iotedge/modules/DS18B2TemperatureReader/SensorData.cs
@@ -0,0 +1,14 @@
+using System;
+using Newtonsoft.Json;
+
+public class SensorData
+{
+    [JsonProperty("sensorid")]
+    public string SensorId { get; set; }
+
+    [JsonProperty("timestamp")]
+    public DateTime MeasurementDateTime { get; set; }
+
+    [JsonProperty("temperature")]
+    public double Temperature { get; set; }
+}
