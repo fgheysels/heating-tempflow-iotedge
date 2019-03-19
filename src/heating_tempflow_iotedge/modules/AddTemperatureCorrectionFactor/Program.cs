ca4585785badb3e16987d434acdb0ebd8de4661a
Frederik Gheysels <frederik.gheysels@telenet.be>
2019-03-19 23:46:28 +0100

    First attempt to report twin properties and respond to changes in desired twin properties

--- src/heating_tempflow_iotedge/modules/AddTemperatureCorrectionFactor/Program.cs
+++ src/heating_tempflow_iotedge/modules/AddTemperatureCorrectionFactor/Program.cs
@@ -1,19 +1,23 @@
 namespace AddTemperatureCorrectionFactor
 {
-    using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Runtime.InteropServices;
     using System.Runtime.Loader;
     using System.Security.Cryptography.X509Certificates;
     using System.Text;
-    using System.Threading;
     using System.Threading.Tasks;
-    using Microsoft.Azure.Devices.Client;
+    using System.Threading;
+    using System;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Shared;
+    using Newtonsoft.Json;
 
     class Program
     {
-        static int counter;
+
+        private static readonly Dictionary<string, SensorCorrectionFactor> _correctionFactors = new Dictionary<string, SensorCorrectionFactor>();
 
         static void Main(string[] args)
         {
@@ -32,7 +36,7 @@ namespace AddTemperatureCorrectionFactor
         public static Task WhenCancelled(CancellationToken cancellationToken)
         {
             var tcs = new TaskCompletionSource<bool>();
-            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>) s).SetResult(true), tcs);
             return tcs.Task;
         }
 
@@ -50,8 +54,69 @@ namespace AddTemperatureCorrectionFactor
             await ioTHubModuleClient.OpenAsync();
             Console.WriteLine("IoT Hub module client initialized.");
 
+            _correctionFactors.Add("test", new SensorCorrectionFactor
+            {
+                SensorId = "test",
+                SensorDescription = "test description",
+                CorrectionFactdor = 7.4f
+            });
+
+            // Register callback to be called when the Desired Properties for this module are updated.
+            await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertiesUpdated, ioTHubModuleClient);
             // Register callback to be called when a message is received by the module
-            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", ApplyCorrectionFactor, ioTHubModuleClient);
+
+            var twin = await ioTHubModuleClient.GetTwinAsync();
+
+            Console.WriteLine(JsonConvert.SerializeObject(twin.Properties));
+
+            await UpdateReportedProperties(ioTHubModuleClient);
+        }
+
+        private static Task<MessageResponse> ApplyCorrectionFactor(Message message, object userContext)
+        {
+            return Task.FromResult(MessageResponse.Completed);
+        }
+
+        private static Task OnDesiredPropertiesUpdated(TwinCollection desiredProperties, object userContext)
+        {
+            if (desiredProperties.Contains("CorrectionFactors") == false)
+            {
+                return Task.CompletedTask;
+            }
+
+            string o = desiredProperties["CorrectionFactors"];
+
+            var correctionFactors = JsonConvert.DeserializeObject<SensorCorrectionFactor[]>(o);
+
+            return Task.CompletedTask;
+        }
+
+        private static async Task UpdateReportedProperties(ModuleClient client)
+        {
+            List<SensorCorrectionFactor> factors = new List<SensorCorrectionFactor>();
+
+            foreach (var kvp in _correctionFactors)
+            {
+                factors.Add(kvp.Value);
+            }
+
+            var reportedProperties = new TwinCollection();
+
+            reportedProperties["CorrectionFactors"] = JsonConvert.SerializeObject(factors);
+
+            try
+            {
+                await client.UpdateReportedPropertiesAsync(reportedProperties);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.Message);
+                foreach (var exception in ex.InnerExceptions)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
         }
 
         /// <summary>
@@ -59,31 +124,38 @@ namespace AddTemperatureCorrectionFactor
         /// It just pipe the messages without any change.
         /// It prints all the incoming messages.
         /// </summary>
-        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
-        {
-            int counterValue = Interlocked.Increment(ref counter);
+        // static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        // {
+        //     int counterValue = Interlocked.Increment(ref counter);
 
-            var moduleClient = userContext as ModuleClient;
-            if (moduleClient == null)
-            {
-                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
-            }
+        //     var moduleClient = userContext as ModuleClient;
+        //     if (moduleClient == null)
+        //     {
+        //         throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+        //     }
 
-            byte[] messageBytes = message.GetBytes();
-            string messageString = Encoding.UTF8.GetString(messageBytes);
-            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+        //     byte[] messageBytes = message.GetBytes();
+        //     string messageString = Encoding.UTF8.GetString(messageBytes);
+        //     Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
 
-            if (!string.IsNullOrEmpty(messageString))
-            {
-                var pipeMessage = new Message(messageBytes);
-                foreach (var prop in message.Properties)
-                {
-                    pipeMessage.Properties.Add(prop.Key, prop.Value);
-                }
-                await moduleClient.SendEventAsync("output1", pipeMessage);
-                Console.WriteLine("Received message sent");
-            }
-            return MessageResponse.Completed;
-        }
+        //     if (!string.IsNullOrEmpty(messageString))
+        //     {
+        //         var pipeMessage = new Message(messageBytes);
+        //         foreach (var prop in message.Properties)
+        //         {
+        //             pipeMessage.Properties.Add(prop.Key, prop.Value);
+        //         }
+        //         await moduleClient.SendEventAsync("output1", pipeMessage);
+        //         Console.WriteLine("Received message sent");
+        //     }
+        //     return MessageResponse.Completed;
+        // }
+    }
+
+    public class SensorCorrectionFactor
+    {
+        public string SensorId { get; set; }
+        public string SensorDescription { get; set; }
+        public float CorrectionFactdor { get; set; }
     }
 }
